0dd4bdce71ca3c18e573048c7f41eaf3fd246942
Andrew Matt <andrewm1246@gmail.com>
2021-04-04 12:33:18 -0400

    Added flask-login and email column to table

--- flaskr/models.py
+++ flaskr/models.py
@@ -1,4 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin, LoginManager
 
 # create our SQLAlchemy db object that will be used to interact with our databa    se
 db = SQLAlchemy()
@@ -9,14 +11,27 @@ class userInformation(db.Model):
 
     id = db.Column(db.BigInteger, primary_key=True)
     username = db.Column(db.String(20), nullable=False, unique = True)
-    password = db.Column(db.String(20), nullable=False)
+    password_hash = db.Column(db.String(), nullable=False)
+    email = db.Column(db.String(50), nullable=False, unique = True)
 
-    def __init__(self, username, password):
+    def __init__(self, username, email):
         self.username = username
-        self.password = password
+        self.email = email
+
+    def set_password(self, password):
+        self.password_hash = generate_pasword_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         return f"{self.username}:{self.password}"
+
+
+@login.user_loader
+def load_user(id):
+    return UserModel.query.get(int(id))
+
 
 class userPostings(db.Model):
     __tablename__ = 'userposts'
