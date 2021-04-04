f6a67739683813dd91195ff5fce358f16db0a893
Andrew Matt <andrewm1246@gmail.com>
2021-04-04 13:28:11 -0400

    added registration with hashes successfully

--- flaskr/models.py
+++ flaskr/models.py
@@ -20,7 +20,7 @@ class userInformation(db.Model):
         self.email = email
 
     def set_password(self, password):
-        self.password_hash = generate_pasword_hash(password)
+        self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
