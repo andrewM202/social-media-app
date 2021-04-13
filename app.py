247e93d55db35698110f70b6a53cff707e7a087f
Andrew Matt <andrewm1246@gmail.com>
2021-04-13 15:19:59 -0400

    added all to root repository

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from models import db, userInformation, userPostings, login
-import config
+from config import Config
 from flask import Flask, render_template
 # Flask migrate is used to connect our flask tables to our postgres database
 from flask_migrate import Migrate
@@ -7,7 +7,7 @@ from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 # The function below is called the application factory function. It returns "app", our flask app, which web hosting services like Heroku can then used to deploy our app. 
-def create_app(config):
+def create_app():
     # Create our flask application object
     app = Flask(__name__)
     # Set the configurations for our flask application to the ones we specified in configs.py
@@ -30,7 +30,8 @@ def create_app(config):
     # return our flask application object
     return app
 
+my_app = create_app()
+
 # run our application with debug mode on
 if __name__ == "__main__":
-    app = create_app(config)
     app.run(debug=True)
