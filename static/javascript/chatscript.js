9f0172b38317d1dc20b7879a96c9b6ccdab03c1a
Andrew Matt <andrewm1246@gmail.com>
2021-05-05 14:12:57 -0400

    update for heroku

--- static/javascript/chatscript.js
+++ static/javascript/chatscript.js
@@ -5,11 +5,11 @@ $(document).ready(function () {
     //let socket = io.connect("http://127.0.0.1:5000");
     //let socket_broadcast = io.connect("http://127.0.0.1:5000/broadcast-message");
     //http://' + document.domain + ':' + location.port + namespace
-    let socket = io.connect("wss://social-media-app421.herokuapp.com");
-    let socket_broadcast = io.connect("wss://social-media-app421.herokuapp.com/broadcast-message");
+    //let socket = io.connect("wss://social-media-app421.herokuapp.com");
+    //let socket_broadcast = io.connect("wss://social-media-app421.herokuapp.com/broadcast-message");
 
-    //let socket = ("https://" + document.domain + ":" + location.port)
-    //let socket_broadcast = ("https://" + document.domain + ":" + location.port + "/broadcast-message")
+    let socket = ("wss://" + document.domain + ":" + location.port)
+    let socket_broadcast = ("wss://" + document.domain + ":" + location.port + "/broadcast-message")
 
     // socket.on to listen for an event. When the server is started, the connect event fires, which is what is happening below
     
