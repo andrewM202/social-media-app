9e132295e22b23486c4180e204e534c5a35cdd88
Lexie Keller <lexiekeller@Lexies-MacBook-Air.local>
2021-05-05 15:25:55 -0400

    Added logout option and CSS for buttons

--- static/JS/script.js
+++ static/JS/script.js
@@ -1,10 +1,19 @@
+//Sign in function for google
 function onSignIn(googleUser) {
   var profile = googleUser.getBasicProfile();
-  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('ID: ' + profile.getId());
   console.log('Name: ' + profile.getName());
   console.log('Image URL: ' + profile.getImageUrl());
-  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  console.log('Email: ' + profile.getEmail());
 }
+
+//Sign out function for google
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
+    });
+  }
 
 window.fbAsyncInit = function() {
   FB.init({
